08ee6b2b0bd40f119b8e264b6ef72182a5eca67e
steve9948 <mbuguagichimu@gmail.com>
2025-07-02 15:04:43 +0300

    Created a script to generate secret keys

--- generate_key.py
+++ generate_key.py
@@ -1,6 +1,7 @@
 import secrets
 import string
 
+#Python script for generating secret keys
 def generate_secret_key():
     chars = string.ascii_lowercase + string.digits + '!@#$%^&*(-_=+)'
     return ''.join(secrets.choice(chars) for _ in range(50))
